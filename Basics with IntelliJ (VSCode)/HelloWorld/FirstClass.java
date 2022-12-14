630e8df07bf3abc0b06a0980345a5dc8fa824ded
Chik Fong Eugene FOO <102475213+erulla@users.noreply.github.com>
2022-12-14 22:55:22 +0800

    update

--- Basics with IntelliJ (VSCode)/HelloWorld/FirstClass.java
+++ Basics with IntelliJ (VSCode)/HelloWorld/FirstClass.java
@@ -1,6 +1,6 @@
 package HelloWorld;
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.print("Hello World");
     } 
-}
+} 
